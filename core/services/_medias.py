bac3a58a0ec49d23fceb90fd5e705a9e131bf057
amitrajputfff <49436537+amitrajputfff@users.noreply.github.com>
2021-12-04 02:05:12 +0530

    Delete core directory

--- core/services/_medias.py
+++
@@ -1,7 +0,0 @@
-from profil3r.modules.medias.medium import Medium
-
-# Medium
-def medium(self):
-    self.result["medium"] = Medium(self.CONFIG, self.permutations_list).search()
-    # print results
-    self.print_results("medium")
